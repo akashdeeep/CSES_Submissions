4dc164ab75d39e369e49a4e4aa99aa1afc216b15
Akash Deep <90370841+akashdeeep@users.noreply.github.com>
2022-09-29 01:53:22 -0400

    Add files via upload

---
+++ Subarray_DIvisibility_sorting_and_searching.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename... T>
+void see(T &...args) { ((cin >> args), ...); }
+template <typename... T>
+void put(T &&...args) { ((cout << args << " "), ...); }
+template <typename... T>
+void putl(T &&...args)
+{
+    ((cout << args << " "), ...);
+    cout << '\n';
+}
+#define error(args...)                           \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << "=" << a << ", ";
+    err(++it, args...);
+}
+#define int long long
+#define pb push_back
+#define F first
+#define S second
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define tiii tuple<int, int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vc vector
+#define L cout << '\n';
+#define E cerr << '\n';
+#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rev(i, a, b) for (int i = a; i > b; --i)
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define setpr(x) cout << setprecision(x) << fixed
+#define sz size()
+#define seea(a, x, y)           \
+    for (int i = x; i < y; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define seev(v, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        v.push_back(x);         \
+    }
+#define sees(s, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        s.insert(x);            \
+    }
+const ll inf = 1LL << 62;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+const ll md = 1000000007;
+
+int dp[200005][20];
+
+void constructST(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        dp[i][0] = a[i];
+    for (int j = 1; j <= 19; j++)
+        for (int i = 0; (i + (1 << j)) <= n; i++)
+            dp[i][j] = min(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+}
+
+signed main()
+{
+    int n, x;
+    cin >> n;
+    int arr[n] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i];
+    }
+    int count = 0;
+    int m[n] = {0};
+    m[0] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        count += m[(arr[i] % n + n) % n];
+        m[((arr[i]) % n + n) % n]++;
+    }
+    cout << count;
+}
