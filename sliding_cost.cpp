f069cfbdb6dbe5ab0704d11d0b203e8172d0fefd
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-03-10 19:01:01 +0530

    sliding cost

---
+++ sliding_cost.cpp
@@ -0,0 +1,193 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename... T>
+void see(T &...args) { ((cin >> args), ...); }
+template <typename... T>
+void put(T &&...args) { ((cout << args << " "), ...); }
+template <typename... T>
+void putl(T &&...args)
+{
+    ((cout << args << " "), ...);
+    cout << '\n';
+}
+#define error(args...)                           \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << "=" << a << ", ";
+    err(++it, args...);
+}
+#define int long long
+#define pb push_back
+#define F first
+#define S second
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define tiii tuple<int, int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vc vector
+#define E cerr << '\n';
+#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rev(i, a, b) for (int i = a; i > b; --i)
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define setpr(x) cout << setprecision(x) << fixed
+#define seea(a, x, y)           \
+    for (int i = x; i < y; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define seev(v, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        v.push_back(x);         \
+    }
+#define sees(s, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        s.insert(x);            \
+    }
+const ll inf = 1LL << 62;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+const ll md = 1000000007;
+
+// tree
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T>
+// using oset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// int dp[200005][20];
+
+// void constructST(int a[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//         dp[i][0] = a[i];
+//     for (int j = 1; j <= 19; j++)
+//         for (int i = 0; (i + (1 << j)) <= n; i++)
+//             dp[i][j] = min(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+// }
+
+// struct edge
+// {
+//     int L;
+//     int R;
+//     int id;
+//     edge(int a, int b, int c)
+//     {
+//         L = a;
+//         R = b;
+//         id = c;
+//     }
+// };
+
+// bool comp(edge a, edge b)
+// {
+//     if (a.L == b.L)
+//         return a.R > b.R;
+//     return a.L < b.L;
+// }
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<int> ans;
+    int m = (k + 1) / 2, b = 0, lsum = 0, rsum = 0;
+    priority_queue<pair<int, int>> l;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> r;
+    rep(i, 0, n)
+    {
+        while (!r.empty() && r.top().S <= i - k)
+            r.pop();
+        while (!l.empty() && l.top().S <= i - k)
+            l.pop();
+        if (b < m)
+        {
+            r.push({arr[i], i});
+            rsum += arr[i];
+            l.push(r.top());
+            lsum += r.top().F;
+            rsum -= r.top().F;
+            r.pop();
+            b++;
+        }
+        else
+        {
+            l.push({arr[i], i});
+            lsum += arr[i];
+            r.push(l.top());
+            rsum += l.top().F;
+            lsum -= l.top().F;
+            l.pop();
+        }
+        while (!r.empty() && r.top().S <= i - k)
+            r.pop();
+        while (!l.empty() && l.top().S <= i - k)
+            l.pop();
+        if (i < k - 1)
+            continue;
+        int med = l.top().F;
+        cout << m * med - lsum + rsum - (k - m) * med << " ";
+        if (arr[i - k + 1] <= l.top().F)
+        {
+            b--;
+            lsum -= arr[i - k + 1];
+        }
+        else
+        {
+            rsum -= arr[i - k + 1];
+        }
+    }
+    cout << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef ONLINEJUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);   // can need to change file . this one for taking input
+    freopen("output.txt", "w", stdout); // this one for output
+#endif
+
+    // int T;
+    // cin >> T;
+    // while (T--)
+    solve();
+
+#ifdef ONLINEJUDGE
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC); // this line gives your code runtime
+#endif
+
+    return 0;
+}
