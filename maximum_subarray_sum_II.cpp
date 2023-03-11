e210cda3431d92920ddef7765d2739b93542b0a5
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-03-11 09:26:29 +0530

    maximum_subarray_sum_II.cpp

---
+++ maximum_subarray_sum_II.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename... T>
+void see(T &...args) { ((cin >> args), ...); }
+template <typename... T>
+void put(T &&...args) { ((cout << args << " "), ...); }
+template <typename... T>
+void putl(T &&...args)
+{
+    ((cout << args << " "), ...);
+    cout << '\n';
+}
+#define error(args...)                           \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << "=" << a << ", ";
+    err(++it, args...);
+}
+#define int long long
+#define pb push_back
+#define F first
+#define S second
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define tiii tuple<int, int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vc vector
+#define E cerr << '\n';
+#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rev(i, a, b) for (int i = a; i > b; --i)
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define setpr(x) cout << setprecision(x) << fixed
+#define seea(a, x, y)           \
+    for (int i = x; i < y; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define seev(v, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        v.push_back(x);         \
+    }
+#define sees(s, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        s.insert(x);            \
+    }
+const ll inf = 1LL << 62;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+const ll md = 1000000007;
+
+// tree
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T>
+// using oset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// int dp[200005][20];
+
+// void constructST(int a[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//         dp[i][0] = a[i];
+//     for (int j = 1; j <= 19; j++)
+//         for (int i = 0; (i + (1 << j)) <= n; i++)
+//             dp[i][j] = min(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+// }
+
+// struct edge
+// {
+//     int L;
+//     int R;
+//     int id;
+//     edge(int a, int b, int c)
+//     {
+//         L = a;
+//         R = b;
+//         id = c;
+//     }
+// };
+
+bool comp(pair<int, int> a, pair<int, int>b)
+{
+    if (a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
+int temp(vector<int> arr, int n, int a) {
+    int curr = 0;
+    int ans = -inf;
+    for (int i = 0 ; i < a ; i++) {
+        curr += arr[i];
+    }
+    ans = max(ans, curr);
+    for (int i = a ; i < n ; i++) {
+        curr += -arr[i - a] + arr[i];
+        ans = max(ans, curr);
+    }
+    return ans;
+}
+
+void solve()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    vector<int> pre(n + 1, 0);
+    for (int i = 1 ; i <= n ; i++) {
+        int x;
+        cin >> x;
+        pre[i] = x + pre[i - 1];
+    }
+    int ans = -inf;
+    multiset<int> ms;
+    for (int i = a; i <= n; i++) {
+        if (i > b) ms.erase(ms.find(pre[i - b - 1]));
+        ms.insert(pre[i - a]);
+        ans = max(ans, pre[i] - *ms.begin());
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef ONLINEJUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);   // can need to change file . this one for taking input
+    freopen("output.txt", "w", stdout); // this one for output
+#endif
+
+    // int T;
+    // cin >> T;
+    // while (T--)
+    solve();
+
+#ifdef ONLINEJUDGE
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC); // this line gives your code runtime
+#endif
+
+    return 0;
+}
